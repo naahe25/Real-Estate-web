8aff5e8120fa02da9365c05d7ffae3a7e5f9abd1
naahe25 <naahebd25@gmail.com>
2024-12-05 09:36:19 +0600

    Client Part Done

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Suspense } from "react";
+import Layout from "./components/Layout/Layout";
+import Website from "./pages/Website";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Properties from "./pages/Properties/Properties";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+            <Route path="/properties" element={<Properties />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
